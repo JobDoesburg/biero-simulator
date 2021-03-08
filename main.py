f28bfd176c284d77744fae97e5605878df7c0f4b
Job Doesburg <job.doesburg@gmail.com>
2021-03-08 19:17:03 +0100

    Update

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import os
 import random
 
 from github import Github
+from tabulate import tabulate
 
 g = Github(os.environ.get("GITHUB_TOKEN"))
-repo = g.get_user().get_repo("technicie-biero")
+repo = g.get_user().get_repo("technicie-biero-test")
 board = repo.get_projects()[0]
 
 columns = board.get_columns()
@@ -55,6 +56,41 @@ titles = [
     "De keel moet gesmeerd worden",
     "HertogJanFactory needs more consumers",
     "throat.py needs more pils",
+    "Drinking issue",
+    "Apply new drinking guidelines",
+    "Introduce Beer API v2",
+    "beer.exe has crashed on AlcoholException",
+    "Loading CraftBeerModule failed, try again",
+    "Add new shot app",
+    "Add Gall&Gall sync",
+    "Kernel panic, switching to shots",
+    "Add @Alcoholic annotations",
+    "Enrich AlcoholPreprocessors",
+    "Poor pils in CI pipeline",
+    "DrinkingBufferOverflow crashes on 500",
+    "Fix Segfault in beer tap",
+    "Implement AlcoholCongestionControl",
+    "Prevent Sentry from reporting laffe borrelaars",
+    "Rewrite app to Flugel",
+    "Fix HTTP 428 I'm a beer crate",
+    "Solve HTTP 413 alcohol payload too large",
+    "Member statistics is missing drinking data",
+    "Add alcohol percentage to statistics page",
+    "Unexpected HTTP 404 drink not found",
+    "Refactor soda with beer in drinking app",
+    "Improve alcohol coverage",
+    "Investigate pycraftbeer library",
+    "Increase alcohol coverage percentage",
+    "Initial Adt",
+    "Remove DRIVINGLICENSE due to alcohol misbehavior",
+    "Add ContentShotPolicy to nginx config",
+    "Update ADTME.md",
+    "Undo alcohol minimization",
+    "ValidationError while parsing new drinks",
+    "Allow beer consumption in future",
+    "Add BeerDocker to crafttab",
+    "Create beer command to bierobot",
+    "Change primary color to gold-fellow-yellow",
 ]
 
 
@@ -75,7 +111,7 @@ def ask_type():
 
 
 def ask_how_many():
-    number = int(input("How many? "))
+    number = int(input("How many? (there are 15 participants) "))
     if not number > 1:
         print("I don't know what to do...")
         return ask_how_many()
@@ -104,29 +140,56 @@ def create():
     number = int(input("How many? "))
 
     if number == 1:
-        create_issue(kind, person)
+        name = input("Give custom name (leave empty for random generation): ")
+        if name == "":
+            name = None
+        create_issue(kind, name=name, assignee=person)
         print(f"Created a {kind.name}{' for '+ str(person.name) if person else ''}🍺")
     else:
         for _ in range(number):
-            create_issue(kind, person)
+            create_issue(kind, assignee=person)
         print(
             f"Created {number} {kind.name}s{' for '+ str(person.name) if person else ''}🍺"
         )
 
 
 def emergency_adt():
+    name = input("Give custom name (leave empty for automatic generation): ")
+    if name == "":
+        name = "CRITICAL BUG HOTFIX 🍻⏱"
     for person in users:
         if person in admins:
             break
         _, card = create_issue(
-            get_type("adt"), "CRITICAL BUG - HOTFIX 🍻⏱", user(person), col=drinking
+            get_type("adt"), name=name, assignee=user(person), col=drinking
         )
     print("🍻⏱ Emergency adt launched for every person.")
+
+def stats():
+    print("Displaying the statistics for every person")
+    output = []
+    headers = ["Name", f"total ({repo.get_issues(state='all').totalCount})", f"open ({repo.get_issues(state='open').totalCount})", f"closed ({repo.get_issues(state='closed').totalCount})"]
+    for person in users:
+        if person in admins:
+            break
+        u = user(person)
+        total = repo.get_issues(assignee=u)
+        open = repo.get_issues(state="open", assignee=u)
+        closed = repo.get_issues(state="closed", assignee=u)
+        output.append([person, total.totalCount, open.totalCount, closed.totalCount])
+
+    total = repo.get_issues(assignee="none")
+    open = repo.get_issues(state="open", assignee="none")
+    closed = repo.get_issues(state="closed", assignee="none")
+    output.append(["unassigned", total.totalCount, open.totalCount, closed.totalCount])
+
+    print(tabulate(output, headers=headers))
 
 
 commands = {
     "Create a new issue 🍺/🥃/🍹": create,
     "Launch an emergency adt 🍻⏱": emergency_adt,
+    "Show statistics on many everyone drank ⁉️": stats,
 }
 
 
@@ -137,7 +200,7 @@ def main():
             print(f"[{i+1}] {x}")
         task = int(input("What do you want to do? "))
         commands[list(commands.keys())[task - 1]]()
-        print("\n")
+        print("")
 
 
 if __name__ == "__main__":
